869fbdb48a2c0986ab44cf810f151dcfbb33118c
maria <mpramos78915400@gmail.com>
2024-05-23 22:01:52 -0400

    formularios2 con objetos usando useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,57 @@
 import { useState } from 'react'
 function App() {
+  const options=[
+    {
+      value:'',
+      label: '--Select Country--'
+    },
+    {
+      value:'Argentina',
+      label: 'Argentina'
+    },
+    {
+      value:'Perú',
+      label: 'Perú'
+    },
+    {
+      value:'Colombia',
+      label: 'Colombia'
+    }
+  ]
   const initialState={
     firstName:'',
     lastName:'',
-    country:'',
+    email:'',
     title:'',
+    country:'',
+    tel:'',
+    dateOfBirth:'',
+    favoriteColor:'',
+    weight:'',
+    gender:'',
+    file:'',
+    bio:'',
+    skills:{
+      html:false,
+      css:false,
+      javascript
+    }
   }
-const [formData,setData]=useState(initialState)
-
-const onChange=(e)=>{
-  const {name,value}=e.target
-  setData({...formData,[name]:value})
-}
-const onSubmit =(e)=>{
-  e.preventDefault()
-  console.log(formData);
-}
-const {firstName,lastName,country,title} = formData
-  return (
+  const [formDate,setFormData]=useState(initialState)
+  const {firstName,lastName,title,country,email,tel,dateOfBirth,favoriteColor,weight,gender,bio}=formDate
+    return (
     <>
     <div>
       <h3>Add Student</h3>
       <form onSubmit={onSubmit}>
-        <div>
-          <input 
-          type="text"
-          name='firstName'
-          placeholder='first Name'
-          value={firstName}
-          onChange={onChange}
-          />
-        </div>
-        <div>
-          <input 
-          type="text"
-          name='lastName'
-          placeholder='last Name'
-          value={lastName}
-          onChange={onChange}
-          />
-        </div>
-        <div>
-          <input 
-          type="text"
-          name='country'
-          placeholder='country'
-          value={country}
-          onChange={onChange}
-          />
-        </div>
-        <div>
-          <input 
-          type="text"
-          name='title'
-          placeholder='title'
-          value={title}
-          onChange={onChange}
-          />
-        </div>
-        <button >Submit</button>
+      <label htmlFor="firstName">First Name</label>
+      <input 
+      type="text"
+      name='firstName'
+      />
       </form>
     </div>
+
     </>
   )
 }
