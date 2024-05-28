ba15870c98b85263e4207be7746fa73b1582e6fb
maria <mpramos78915400@gmail.com>
2024-05-27 20:52:35 -0400

    enlaces

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Menu = () => {
   return (
     <nav>
         <ul>
             <li>
-              <Link to="/" >Home</Link>
+              {/* <Link to="/" >Home</Link> */}
+              <NavLink 
+              style={({isActive})=>({color: isActive?'red':'blue'})}
+              to="/"
+              >Home</NavLink>
             </li>
             <li>
-              <Link to="/blog" >Blog</Link>
+              {/* <Link to="/blog" >Blog</Link> */}
+              <NavLink 
+              style={({isActive})=>({color: isActive?'red':'blue'})}
+              to="/blog"
+              >Blog</NavLink>
             </li>
             <li>
-              <Link to="/profile" >Profile</Link>
+              {/* <Link to="/profile" >Profile</Link> */}
+              <NavLink 
+              style={({isActive})=>({color: isActive?'red':'blue'})}
+              to="/profile"
+              >Profile</NavLink>
             </li>
 
         </ul>
